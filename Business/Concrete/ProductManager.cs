d7c70ffbfc02febca032598bcd441126b3bce40f
mirayy demir <miray_Demir94@hotmail.com>
2021-02-05 21:06:19 +0300

    03.01

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -10,14 +10,26 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
+        //iş kodları
+        //yetkisi var mı? gibi.
         IProductDal _productDal;
-        public ProductManager (IProductDal productDal)
+        public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
         }
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
